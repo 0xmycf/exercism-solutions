76cde286a9c639812a768ec7cad0172ac59122f1
0xmycf <mycf.mycf.mycf@gmail.com>
2024-04-27 15:03:41 +0200

    pandgram slight improvement

--- julia/pangram/pangram.jl
+++ julia/pangram/pangram.jl
@@ -7,17 +7,13 @@ Return `true` if `input` contains every alphabetic character (case insensitive).
 function ispangram(input)
     mask = 0
     check = 0
-    for l in "abcdefghijklmnopqrstuvwxyz"
-        l = uppercase(l)
+    for l in 'A':'Z'
         check |= (1 << (codepoint(l) - codepoint('A')))
     end
     for l in input
         l = uppercase(l)
         mask |= (1 << (codepoint(l) - codepoint('A')))
     end
-    if check == (mask & check)
-        return true
-    end
-    return false
+    return check == (mask & check)
 end
 
