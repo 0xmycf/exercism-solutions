c21e6203440b133ff6c61e79bcd43205105eda09
0xmycf <mycf.mycf.mycf@gmail.com>
2024-08-21 01:18:20 +0200

    racket falke is still not working

--- racket/flake.nix
+++ racket/flake.nix
@@ -26,21 +26,24 @@ must ask nixos community for help
           #   name = "DYLD_LIBRARY_PATH";
           #   paths = [prevAttrs.LD_LIBRARY_PATH (final.lib.makeLibraryPath [final.xorg.libX11])];
           # }; # ++ prevAttrs.libPath;
-          DYLD_LIBRARY_PATH = "${final.xorg.libX11}/lib";
+          DYLD_LIBRARY_PATH = "${final.xorg.libX11}";
           configureFlags = prevAttrs.configureFlags ++ ["--enable-prefix"];
         });
       };
       myPkgs = pkgs.extend overlay;
+
     in {
       devShells = {
         default = pkgs.mkShell {
           name = "racket shell";
           buildInputs = with pkgs; [racket];
-
+          LD_LIBRARY_PATH   = "${pkgs.xorg.libX11}";
+          DYLD_LIBRARY_PATH = "${pkgs.xorg.libX11}";
           shellHook = ''
-            echo "run raco pkg install racket-langserver"
+            echo "run 'raco pkg install racket-langserver'"
+            echo "nothing works here, dont use this flake D:"
             # raco pkg install racket-langserver
-            export DYLD_LIBRARY_PATH="${pkgs.xorg.libX11}";
+            # export ;
           '';
         };
       };
